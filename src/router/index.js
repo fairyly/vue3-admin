9d731fd8e512cab4fc2a3e3f5d8ff07ccad7dfc5
yuehq <yuehq@helianhealth.com>
2022-01-24 23:20:44 +0800

    feat: 初始化项目

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: '',
+    component: () => import(/* webpackChunkName: "layout" */ '@/layout/index.vue'),
+    meta: {
+      title: '首页',
+    },
+  },
+  { path: '/login', component: () => import('@/views/login/index.vue') },
+]
+
+// 3. 创建路由实例并传递 `routes` 配置
+const router = createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: createWebHashHistory(),
+  routes, // `routes: routes` 的缩写
+})
+
+export default router
